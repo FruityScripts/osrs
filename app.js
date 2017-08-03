733472aeccdfc7eccf4837f94c0a81da1bcbb450
fruity <itsbenmason@gmail.com>
2017-08-03 19:23:41 +0100

    amends to fk up

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 3000;
 const osrsge = require("./osrs-ge/osrs-ge.js");
 
 const dbAdd = "mongodb://heroku_7dl5lkqv:ukou49hgjq420huiga2stqspc5@ds157258.mlab.com:57258/heroku_7dl5lkqv";
-const express = require('expresss');
+const express = require('express');
 const { MongoClient, ObjectID } = require("mongodb");
 
 
